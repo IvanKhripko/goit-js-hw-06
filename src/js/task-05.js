89835f2863b271fc02dd87a363ea13ca46cdb9ff
IvanKhripko <79042938+IvanKhripko@users.noreply.github.com>
2022-01-18 16:01:14 +0200

    Task 5 Task 9
    добавил комментарий в 5 задачу.
    Написал скрипт, который изменяет цвет фона

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,3 +1,8 @@
+/*
+<input type="text" id="name-input" placeholder="Please enter your name" />
+<h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+*/
+
 const inputEl = document.querySelector('#name-input');
 const nameOutputEl = document.querySelector('#name-output');
 
